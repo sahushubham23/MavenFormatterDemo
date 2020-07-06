d18535439897386cc7ae768a1cd66bd2995d4540
Shubham Sahu <shubham.s@aitglobalinc.com>
2020-07-06 13:41:06 +0530

    Compiler Changed from 8 to 11. Added support for blank_lines_after_imports configuration.

--- src/main/java/DemoClass.java
+++ src/main/java/DemoClass.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
+
 class DemoClass {
 
 	public static void main(String[] args) {
@@ -6,7 +14,27 @@ class DemoClass {
 
 
 
+		ArrayList<String> testList = new ArrayList<String>();
+		Map<String, String> testMap = new HashMap<String, String>();
 		System.out.println("First Print testing");
+		int i = 0;
 		System.out.println("Second Print testing");
+
+		if (1 == 1)
+			System.out.println("Conditional Formatting Testing");
+		else
+			System.out.println("Else Formatting Testing");
+
+	}
+
+	public static String ifFormatterTest() {
+
+
+
+
+
+		if (1 == 1)
+			return "Testing Completed";
+		return  "";
 	}
 }
